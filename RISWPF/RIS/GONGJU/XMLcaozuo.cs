59e750d5363be87b2b12caef52ef8639761d98ee
piaoyaocdl <piaoyaocdl@163.com>
2015-12-17 15:39:52 +0800

    dwdwq

---
+++ RISWPF/RIS/GONGJU/XMLcaozuo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Win32;
+
+namespace RIS.GONGJU
+{
+    public class XMLcaozuo
+    {
+        public static XElement FindByNeirong(XElement root, string neirong)
+        {
+            foreach (var jiedian in root.Descendants())
+            {
+                if (jiedian.Descendants().Count() == 0 && jiedian.Value.Equals(neirong))
+                {
+                    return jiedian;
+                }
+            }
+            return null;
+        }
+
+        public static XElement FindById(XElement root, string idneirong)
+        {
+            foreach (var jiedian in root.Descendants())
+            {
+                if (jiedian.Attribute("id") != null && jiedian.Attribute("id").Value.Equals(idneirong))
+                {
+                    return jiedian;
+                }
+            }
+            return null;
+        }
+
+        public static void BaocunXML(XElement root, string wenjianming)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.ValidateNames = true;
+            sfd.FileName = wenjianming;
+
+            var yaoxiazai = (bool)sfd.ShowDialog();
+
+            if (yaoxiazai)
+            {
+                using (var ls = sfd.OpenFile())
+                {
+                    string xmlbanben = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<?mso-application progid = \"Word.Document\" ?> \n ";
+                    var Wendang = Encoding.UTF8.GetBytes(xmlbanben+  root.ToString());
+                    ls.Write(Wendang, 0, Wendang.Length);
+                }
+            }
+        }
+    }
+}
---
+++ RISWPF/RIS/REPORT/HLA.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RIS.REPORT
+{
+    public class HLA
+    {
+        public static void Dayinbaobiao( PAGE.ZUZHIPEIXING.LINCHUANGHLA.JIANCESHENQINGDAN.Zuzhipeixing_linchuanghla_shenqingdanSet xuanzedeshenqingdan  )
+        {
+            XElement moban = XElement.Parse(RIS.Properties.Resources.HLA);
+            GONGJU.XMLcaozuo.FindByNeirong(moban, "患者姓名替换").Value = xuanzedeshenqingdan.huanzhexingming;
+            GONGJU.XMLcaozuo.FindByNeirong(moban, "性别替换").Value = xuanzedeshenqingdan.xingbie;
+            GONGJU.XMLcaozuo.FindByNeirong(moban, "出生日期替换").Value = xuanzedeshenqingdan.chushengriqi.ToString().Substring(0, 10);
+            GONGJU.XMLcaozuo.FindByNeirong(moban, "申请单编号替换").Value = xuanzedeshenqingdan.shenqingdanbianhao;
+
+            var yangbenhang = GONGJU.XMLcaozuo.FindById(moban, "yangbenhang");
+            foreach (var yangben in xuanzedeshenqingdan.yangbens)
+            {
+                var ls = XElement.Parse(yangbenhang.ToString());
+                GONGJU.XMLcaozuo.FindByNeirong(ls, "样本编号替换").Value = yangben.yangbenbianhao;
+                GONGJU.XMLcaozuo.FindByNeirong(ls, "姓名替换").Value = yangben.xingming;
+                GONGJU.XMLcaozuo.FindByNeirong(ls, "性别替换").Value = yangben.xingbie;
+                GONGJU.XMLcaozuo.FindByNeirong(ls, "共患关系替换").Value = yangben.gonghuanguanxi;
+                yangbenhang.AddAfterSelf(ls);
+            }
+            yangbenhang.Remove();
+            GONGJU.XMLcaozuo.BaocunXML(moban, xuanzedeshenqingdan.huanzhexingming + "-" + xuanzedeshenqingdan.shenqingdanbianhao + ".doc");
+        }
+    }
+}
